b7ffdad263469e41e7f764faacb6661082f991fd
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2024-07-23 12:58:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void InsertionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 12, 11, 13, 5, 6 };
+
+        Console.WriteLine("Original array:");
+        PrintArray(numbers);
+
+        InsertionSort(numbers);
+
+        Console.WriteLine("Sorted array:");
+        PrintArray(numbers);
+    }
+}
